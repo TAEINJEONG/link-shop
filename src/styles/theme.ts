97dfc4538913c55259e0fe6888e772598532ceaf
TAEINJEONG <taeinjeong46@gmail.com>
2025-02-18 20:28:29 +0900

    chore: 폰트, 색상 컴포넌트 생성 및 이미지 assets 저장

---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+  colors: {
+    black: "#14151A",
+    gray300: "#888790",
+    gray200: "#A2A2AC",
+    gray100: "#DDDCDF",
+    white200: "#FAFAFB",
+    white100: "#FAFAFB",
+    white: "#FFFFFF",
+    brandRed: "#FB545B",
+    brandBlue: "#3E45EC",
+  },
+
+  font: {
+    heading: {
+      h1: { size: "20px", weight: "700", lineHeight: "28px" }, // Bold
+    },
+    body: {
+      b18: { size: "18px", weight: "500", lineHeight: "24px" }, // Medium
+      r18: { size: "18px", weight: "400", lineHeight: "24px" }, // Regular
+      sb17: { size: "17px", weight: "600", lineHeight: "22px" }, // SemiBold
+      m17: { size: "17px", weight: "500", lineHeight: "22px" }, // Medium
+      r17: { size: "17px", weight: "400", lineHeight: "22px" }, // Regular
+    },
+    caption: {
+      b16: { size: "16px", weight: "700", lineHeight: "20px" }, // Bold
+      sb16: { size: "16px", weight: "600", lineHeight: "20px" }, // SemiBold
+      m16: { size: "16px", weight: "500", lineHeight: "20px" }, // Medium
+      r16: { size: "16px", weight: "400", lineHeight: "20px" }, // Regular
+      sb15: { size: "15px", weight: "600", lineHeight: "18px" }, // SemiBold
+      m15: { size: "15px", weight: "500", lineHeight: "18px" }, // Medium
+      sb14: { size: "14px", weight: "600", lineHeight: "16px" }, // SemiBold
+    },
+  },
+};
